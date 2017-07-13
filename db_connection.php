3e7db3bbd3d18347992529cf9b881ed3cc2bd3f2
Joven Novo <novo.joven@gmail.com>
2017-07-13 22:25:25 +0800

    database conneciton
    database connection

--- db_connection.php
+++ db_connection.php
@@ -1,1 +1,19 @@
-database connection
+<?php
+$servername = "localhost";
+$user = "root";
+$pass = "";
+
+try {
+    $dbConnection = new PDO("mysql:host=$servername;dbname=flockers", $user, $pass);
+    // set the PDO error mode to exception
+    $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    throw new Exception ("could not find database...");
+   
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
+
+
+?>
