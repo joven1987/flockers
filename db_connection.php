bdb964ae13610edf2a6fb5b990cef4b270aa24bf
joven1987 <novo.joven@gmail.com>
2017-07-14 11:47:23 +0800

    landing page added to landing_page sub folder

--- db_connection.php
+++ db_connection.php
@@ -7,7 +7,7 @@ try {
     $dbConnection = new PDO("mysql:host=$servername;dbname=flockers", $user, $pass);
     // set the PDO error mode to exception
     $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    throw new Exception ("could not find database...");
+
    
 }
 catch(PDOException $e)
